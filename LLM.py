505a32846ddbed2cf4e7dc23b74bbefe102f7fee
Ant832 <antnyjank@gmail.com>
2024-02-09 18:08:08 -0700

    Updated for multiple messages

--- LLM.py
+++ LLM.py
@@ -1,11 +1,17 @@
 from gradio_client import Client
 
 client = Client("https://b90d-161-28-242-155.ngrok-free.app/")
-result = client.predict(
-		"How are you?",	# str  in 'Message' Textbox component
+messages = []
+user_in = input("User: ")
+while user_in != "quit":
+	result = client.predict(
+		
+		messages.append(user_in),	# str  in 'Message' Textbox component
 		'LLM Chat',	# Literal[Query Docs, Search in Docs, LLM Chat]  in 'Mode' Radio component
 		["https://github.com/gradio-app/gradio/raw/main/test/test_files/sample_file.pdf"],	# List[filepath]  in 'Upload File(s)' Uploadbutton component
 		"Chatbot",	# str  in 'System Prompt' Textbox component
 							api_name="/chat"
-)
-print(result)
+	)
+	messages.append(result)
+	print(result)
+	user_in = input("User: ")
