5ba8883065d04a9df083cbd07f721d4e3f51359f
Enoch <enochgillman@gmail.com>
2024-02-12 21:28:12 -0700

    Changes related to testing

--- UVSim/UVSim.py
+++ UVSim/UVSim.py
@@ -16,62 +16,118 @@ def SplitData(input):
         return None
     return data
 
-
 #Switches cases based on the sign and first two integers of the input. This has already been split in the SplitData function
 def CaseSwitch(case, memoryLocation):
     if (case == "+10"):
+        pass
+        #TODO
         Read(memoryLocation)
     elif (case == "+11"):
+        pass
+        #TODO
         Write(memoryLocation)
     elif (case == "+20"):
+        pass
+        #TODO
         Load(memoryLocation)
     elif (case == "+21"):
+        pass
+        #TODO
         Store(memoryLocation)
     elif (case == "+30"):
+        pass
+        #TODO
         Add(memoryLocation)
     elif (case == "+31"):
+        pass
+        #TODO
         Subtract(memoryLocation)
     elif (case == "+32"):
+        pass
+        #TODO
         Divide(memoryLocation)
     elif (case == "+33"):
+        pass
+        #TODO
         Multiply(memoryLocation)
     elif (case == "+40"):
+        pass
+        #TODO
         Branch(memoryLocation)
     elif (case == "+41"):
+        pass
+        #TODO
         BranchNeg(memoryLocation)
     elif (case == "+42"):
+        pass
+        #TODO
         BranchZero(memoryLocation)
     elif (case == "+43"):
+        pass
+        #TODO
         Halt(memoryLocation)
     else:
         return ValueError
 """
 TODO - All of the functions below still need definitions. -Dan
 """
+
 #I/O operators
+#TODO
 def Read(memoryLocation):
+    pass
     
+#TODO
 def Write(memoryLocation):
+    pass
 
 #Load/Store operators
+#TODO
 def Load(memoryLocation):
+    pass
+
+#TODO
 def Store(memoryLocation):
+    pass
 
 #Arithmatic operators
+#TODO
 def Add(memoryLocation):
+    pass
+
+#TODO
 def Subtract(memoryLocation):
+    pass
+
+#TODO
 def Divide(memoryLocation):
+    pass
+
+#TODO
 def Multiply(memoryLocation):
+    pass
 
 #Control operators
+#TODO
 def Branch(memoryLocation):
+    pass
+
+#TODO
 def BranchNeg(memoryLocation):
+    pass
+
+#TODO
 def BranchZero(memoryLocation):
+    pass
+
+#TODO
 def Halt():
+    pass
 
 def main():
     userInput = GetUserInput()
     while userInput != "":
         inputData = SplitData(userInput)
 
-main()
+if __name__ == '__main__':
+    main()
---
+++ UVSim/test_UVSim.py
@@ -0,0 +1,15 @@
+import pytest
+import UVSim
+
+# @pytest.mark.parametrize('example,num', [
+#     ('examplearg1', 1),
+#     ('examplearg2', 2),
+#     ('examplearg3', 3),
+#     ('examplearg4', 4),
+#     ('examplearg5', 5),
+#     ('examplearg6', 6),
+#     ('examplearg7', 7)
+# ])
+
+# def test_example(example,num):
+#     assert int(example[-1]) == num
