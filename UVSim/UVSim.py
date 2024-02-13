1187b970c24f81a6abf35637c8937c8fc6ed83ff
Ant832 <antnyjank@gmail.com>
2024-02-12 22:13:09 -0700

    Added UVSim Class and removed LLM.py

--- UVSim/UVSim.py
+++ UVSim/UVSim.py
@@ -1,75 +1,79 @@
-#Global variables
-memory = [0] * 100
-register = ""
+class UVSim:
+    def __init__(self) -> None:
+        self.memory = [0] * 100
+        self.register = ""
 
-#Simple function that gets user input as a string.
-def GetUserInput():
-    userInput = str(input(" "))
-    return userInput
 
-#Splits the input into a tuple: case, and memory location. Has built in check for correct input length and correct sign.
-def SplitData(input):
-    if (input.length > 5 or input.length < 5):
-        return ValueError
-    data = input[:3], input[3:]
-    if (data[0][0] != "+"):
-        return None
-    return data
+    #Simple function that gets user input as a string.
+    def GetUserInput(self):
+        userInput = str(input(" "))
+        return userInput
 
-#Switches cases based on the sign and first two integers of the input. This has already been split in the SplitData function
-def CaseSwitch(case, memoryLocation):
-    if (case == "+10"):
-        pass
-        #TODO
-        Read(memoryLocation)
-    elif (case == "+11"):
-        pass
-        #TODO
-        Write(memoryLocation)
-    elif (case == "+20"):
-        pass
-        #TODO
-        Load(memoryLocation)
-    elif (case == "+21"):
-        pass
-        #TODO
-        Store(memoryLocation)
-    elif (case == "+30"):
-        pass
-        #TODO
-        Add(memoryLocation)
-    elif (case == "+31"):
-        pass
-        #TODO
-        Subtract(memoryLocation)
-    elif (case == "+32"):
-        pass
-        #TODO
-        Divide(memoryLocation)
-    elif (case == "+33"):
-        pass
-        #TODO
-        Multiply(memoryLocation)
-    elif (case == "+40"):
-        pass
-        #TODO
-        Branch(memoryLocation)
-    elif (case == "+41"):
-        pass
-        #TODO
-        BranchNeg(memoryLocation)
-    elif (case == "+42"):
-        pass
-        #TODO
-        BranchZero(memoryLocation)
-    elif (case == "+43"):
-        pass
-        #TODO
-        Halt(memoryLocation)
-    else:
-        return ValueError
+    #Splits the input into a tuple: case, and memory location. Has built in check for correct input length and correct sign.
+    def SplitData(self, input):
+        if (len(input) > 5 or len(input) < 5):
+            return ValueError
+        data = input[:3], input[3:]
+        if (data[0][0] != "+"):
+            return None
+        return data
+
+    #Switches cases based on the sign and first two integers of the input. This has already been split in the SplitData function
+    def CaseSwitch(self, case, memoryLocation):
+        if (case == "+10"):
+            pass
+            #TODO
+            Read(memoryLocation)
+        elif (case == "+11"):
+            pass
+            #TODO
+            Write(memoryLocation)
+        elif (case == "+20"):
+            pass
+            #TODO
+            Load(memoryLocation)
+        elif (case == "+21"):
+            pass
+            #TODO
+            Store(memoryLocation)
+        elif (case == "+30"):
+            pass
+            #TODO
+            Add(memoryLocation)
+        elif (case == "+31"):
+            pass
+            #TODO
+            Subtract(memoryLocation)
+        elif (case == "+32"):
+            pass
+            #TODO
+            Divide(memoryLocation)
+        elif (case == "+33"):
+            pass
+            #TODO
+            Multiply(memoryLocation)
+        elif (case == "+40"):
+            pass
+            #TODO
+            Branch(memoryLocation)
+        elif (case == "+41"):
+            pass
+            #TODO
+            BranchNeg(memoryLocation)
+        elif (case == "+42"):
+            pass
+            #TODO
+            BranchZero(memoryLocation)
+        elif (case == "+43"):
+            pass
+            #TODO
+            Halt(memoryLocation)
+        else:
+            return ValueError
+
 """
 TODO - All of the functions below still need definitions. -Dan
+     - Add each function as method to class. -Anthony
 """
 
 #I/O operators
@@ -125,9 +129,10 @@ def Halt():
     pass
 
 def main():
-    userInput = GetUserInput()
+    sim = UVSim()
+    userInput = sim.GetUserInput()
     while userInput != "":
-        inputData = SplitData(userInput)
+        inputData = sim.SplitData(userInput)
 
 if __name__ == '__main__':
     main()
