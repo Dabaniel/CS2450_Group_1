b50ea363d797860993348d94c09fefae23bbdeb4
Enoch <enochgillman@gmail.com>
2024-02-12 21:28:44 -0700

    New file

---
+++ UVSim/operations.py
@@ -0,0 +1,15 @@
+#THIS FILE IS MERELY A CONCEPT
+
+def Read(memoryLocation):
+    pass
+
+def Write(memoryLocation):
+    pass
+
+#('code name', function)
+op = {
+    #Read
+    ("+10", Read),
+    #Write
+    ("+11", Write)
+}
