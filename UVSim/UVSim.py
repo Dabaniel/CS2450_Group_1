95dc2c428d0829770c03dccf05d7fc3488bc8191
PrincessLaSwerv <149519589+PrincessLaSwerv@users.noreply.github.com>
2024-02-13 19:44:57 -0700

    Update UVSim.py
    just added a comment to the add from the instruction in canvas

--- UVSim/UVSim.py
+++ UVSim/UVSim.py
@@ -88,6 +88,7 @@ TODO - All of the functions below still need definitions. -Dan
 #Arithmatic operators
 #TODO
 def Add(memoryLocation):
+    #ADD = 30 Add a word from a specific location in memory to the word in the accumulator (leave the result in the accumulator)
     pass
 
 #TODO
