267ba9ea2e2e56b6381a5c5f45d76e81f715155b
Enoch <enochgillman@gmail.com>
2024-02-14 15:37:33 -0700

    changes related to design coordination

--- UVSim/operations.py
+++ UVSim/operations.py
@@ -1,87 +1,75 @@
-#THIS FILE WAS USED TO TEST A 2D LIST AGAINST PYTHON'S DICTIONARY
-#THE PYTHON DICTIONARY IS SLIGHTLY FASTER
+#OPERATIONS CLASS
 
-#I/O operators
-#TODO
-def Read(memoryLocation):
-    pass
-    
-#TODO
-def Write(memoryLocation):
-    pass
+class Operations:
+    def __init__(self) -> None:
+        self.operations = {
+            "+10": self.Read,
+            "+11": self.Write,
+            "+20": self.Load,
+            "+21": self.Store,
+            "+30": self.Add,
+            "+31": self.Subtract,
+            "+32": self.Divide,
+            "+33": self.Multiply,
+            "+40": self.Branch,
+            "+41": self.BranchNeg,
+            "+42": self.BranchZero,
+            "+43": self.Halt
+        }
+    #I/O operators
+    #TODO
+    def Read(memoryLocation):
+        pass
+        
+    #TODO
+    def Write(memoryLocation):
+        pass
 
-#Load/Store operators
-#TODO
-def Load(memoryLocation):
-    pass
+    #Load/Store operators
+    #TODO
+    def Load(memoryLocation):
+        pass
 
-#TODO
-def Store(memoryLocation):
-    pass
+    #TODO
+    def Store(memoryLocation):
+        pass
 
-#Arithmatic operators
-#TODO
-def Add(memoryLocation):
-    pass
+    #Arithmatic operators
+    #TODO
+    def Add(memoryLocation):
+        pass
 
-#TODO
-def Subtract(memoryLocation):
-    pass
+    #TODO
+    def Subtract(memoryLocation):
+        pass
 
-#TODO
-def Divide(memoryLocation):
-    pass
+    #TODO
+    def Divide(memoryLocation):
+        pass
 
-#TODO
-def Multiply(memoryLocation):
-    pass
+    #TODO
+    def Multiply(memoryLocation):
+        pass
 
-#Control operators
-#TODO
-def Branch(memoryLocation):
-    pass
+    #Control operators
+    #TODO
+    def Branch(memoryLocation):
+        pass
 
-#TODO
-def BranchNeg(memoryLocation):
-    pass
+    #TODO
+    def BranchNeg(memoryLocation):
+        pass
 
-#TODO
-def BranchZero(memoryLocation):
-    pass
+    #TODO
+    def BranchZero(memoryLocation):
+        pass
 
-#TODO
-def Halt():
-    pass
-
-#('code name', function)
-test_dict = {
-    "+10": Read,
-    "+11": Write,
-    "+20": Load,
-    "+21": Store,
-    "+30": Add,
-    "+31": Subtract,
-    "+32": Divide,
-    "+33": Multiply,
-    "+40": Branch,
-    "+41": BranchNeg,
-    "+42": BranchZero,
-    "+43": Halt
-}
-
-test_list = [
-    [Read, Write],
-    [Load, Store],
-    [Add, Subtract, Divide, Multiply],
-    [Branch, BranchNeg, BranchZero, Halt]
-]
-
-def accessList(opcode):
-    return test_list[int(opcode[1]) - 1][int(opcode[2])]
+    #TODO
+    def Halt():
+        pass
 
 def main():
-    print(accessList('+10'))
-    print(test_dict['+10'])
+    pass
 
 if __name__ == '__main__':
     main()
