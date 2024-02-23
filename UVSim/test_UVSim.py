1cb205704fe3b0e11e9b61a307d2212e45f8e970
Enoch <enochgillman@gmail.com>
2024-02-22 18:38:58 -0700

    started certain test cases

--- UVSim/test_UVSim.py
+++ UVSim/test_UVSim.py
@@ -6,10 +6,15 @@ def test_get_memory():
     assert type(sim.get_memory()) == type([])
     assert sim.get_memory() == [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None]
 
-def test_Read():
+def test_get_accumulator():
     sim = UVSim.UVSim()
     assert type(sim.get_accumulator()) == type([])
     assert sim.get_accumulator() == [0, '0']
+
+# def test_proceeding_accumulator():
+#     sim = UVSim.UVSim()
+    
+#     assert sim.get_accumulator() == [1, '0']
 
 # @pytest.mark.parametrize('example,num', [
 #     ('examplearg1', 1),
